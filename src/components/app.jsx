235dd578f97d6ffd854402b85b1e997e0c602e41
Marco <maidh91@gmail.com>
2025-03-02 23:56:24 +1100

    setup shadcn

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function App () {
   return (
     <div className="p-[1rem]">
       <div className="h-[1rem]"><pear-ctrl /></div>
       <div className="text-blue-700">Akaoting</div>
+      <Button>Click me</Button>
     </div>
   )
 }
