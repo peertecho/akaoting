75d8010c9acf5b30d0e30a7163f3ae08a52c9e89
Marco <maidh91@gmail.com>
2025-03-03 00:18:48 +1100

    sample table

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,64 @@
+import { 
+  Breadcrumb, 
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator, 
+} from "@/components/ui/breadcrumb";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 import { Button } from "@/components/ui/button";
 
 export default function App () {
   return (
     <div className="p-[1rem]">
       <div className="h-[1rem]"><pear-ctrl /></div>
-      <div className="text-blue-700">Akaoting</div>
-      <Button>Click me</Button>
+
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink>Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink>Account</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Company</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Number</TableHead>
+            <TableHead>Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>Akaoting</TableCell>
+            <TableCell>123456</TableCell>
+            <TableCell>
+              <Button>Open</Button>
+              <Button variant="destructive">Delete</Button>
+            </TableCell>
+            </TableRow>
+        </TableBody>
+      </Table>
+
+      <Button>Add</Button>
     </div>
   )
 }
