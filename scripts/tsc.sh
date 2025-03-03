261ae5664e99f5c4ee10885ab1b14ca780c69024
Marco <maidh91@gmail.com>
2025-03-04 10:43:51 +1100

    tsc

--- scripts/tsc.sh
+++ scripts/tsc.sh
@@ -5,7 +5,7 @@ echo
 echo "Building types..."
 for lib in b4a hypercore-crypto hyperswarm pear-interface; do
   echo $lib
-  rm -rf $rootdir/node_modules/$lib/**/*.d.ts
+  rm -rf $rootdir/node_modules/$lib/*.d.ts $rootdir/node_modules/$lib/**/*.d.ts
   npx tsc --allowJs --declaration --emitDeclarationOnly $rootdir/node_modules/$lib/*.js $rootdir/node_modules/$lib/**/*.js > /dev/null 2>&1 &
 done
 wait
